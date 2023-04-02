ddf58b4e6f86205eaa14e49525233adb28ee62a2
dikiogres <dikiogres@gmail.com>
2023-04-03 00:03:02 +0700

    chore: firebase

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNBqJr9FGMmwgmyHAHN_7EvSfkDP4N7k8",
+    authDomain: "fp-collection-eef4c.firebaseapp.com",
+    projectId: "fp-collection-eef4c",
+    storageBucket: "fp-collection-eef4c.appspot.com",
+    messagingSenderId: "579825109897",
+    appId: "1:579825109897:web:c95cbb17a46b7e67adef58",
+    measurementId: "G-KN6NK1WZCP"
+  };
+
+const app = initializeApp(firebaseConfig);
+
